9ae297177cb2a6017b95a2399aa35451b7fe0274
dweng <darrel.weng@gmail.com>
2018-10-27 21:53:30 -0700

    add pytest unit tests to example_pkg

---
+++ example_pkg/tests/test_foo.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for example_pkg.foo
+"""
+
+import pytest
+
+from example_pkg import foo
+
+
+def test_hello_world():
+    """
+    """
+    assert foo.hello_world() == "Hello World"
+
